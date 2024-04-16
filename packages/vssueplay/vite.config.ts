a1c2002ae195dacc7b309198983e063f568c2334
shellingfordly <shellingfordly@qq.com>
2024-04-16 17:45:13 +0800

    chore: init comps project of vssueplay

---
+++ packages/vssueplay/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/main.ts",
+      formats: ["es"],
+      name: "Vssueplay",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          Vue: "vue",
+        },
+      },
+    },
+  },
+  plugins: [vue(), dts()],
+});
