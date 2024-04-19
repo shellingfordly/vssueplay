3755d7c714fd471945c52fdb97009d9e08449d2c
shellingfordly <shellingfordly@qq.com>
2024-04-19 14:21:12 +0800

    chore: add iconify icons for tailwind

--- packages/vssueplay/tailwind.config.cjs
+++ packages/vssueplay/tailwind.config.cjs
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
+
 module.exports = {
   darkMode: ["selector"],
   content: ["**/*/index.html", "**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // Iconify plugin
+    addDynamicIconSelectors(),
+  ],
 };
